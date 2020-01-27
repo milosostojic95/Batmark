a73e8442d81b3fb33342d5a5737306df2a7ec893
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-01-27 12:14:07 +0100

    Changed background, added wigrum font

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   plugins:[
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/template.html'
+      template: './src/index.html'
     }),
     new HtmlWebpackPlugin({
       filename: 'service.html',
