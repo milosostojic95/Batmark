c0e9da2f20f9a06423dc6e6fb43f4c5ade2bd83b
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-01-31 13:53:01 +0100

    Added text animation

--- src/js/index.js
+++ src/js/index.js
@@ -98,8 +98,57 @@ menuBtn.addEventListener('click', ()=>{
 
 
 
-// type metod
 window.addEventListener('load',() =>{
   const preloader = document.querySelector('.preloader');
   preloader.classList.add('loader-finish');
+});
+
+
+//text animation
+const typeWriter = function(txtElement, words, wait = 2000) {
+  this.txtElement = txtElement;
+  this.txt =  '';
+  this.words = words;
+  this.wordIndex = 0;
+  this.isDeleting = false;
+  this.type();
+  this.wait = wait;
+}
+
+typeWriter.prototype.type = function () {
+  const current = this.wordIndex % this.words.length;
+  const fullTxt = this.words[current];
+  let typeSpeed = 100;
+
+  if(this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+  if(this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  if(!this.isDeleting && this.txt === fullTxt) {
+    typeSpeed = this.wait;
+    this.isDeleting = true;
+  }
+  else if(this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.wordIndex ++;
+    typeSpeed = 500;
+  }
+
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  setTimeout(() =>this.type(), typeSpeed);
+}
+
+
+document.addEventListener('DOMContentLoaded',() => {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+
+  new typeWriter(txtElement, words, wait);
 });
