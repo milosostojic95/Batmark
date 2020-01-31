7990d38af4f6c7ebc415be4aab94c4ac15f73e5a
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-01-31 14:45:23 +0100

    Added scroll animation

--- src/js/index.js
+++ src/js/index.js
@@ -153,3 +153,25 @@ document.addEventListener('DOMContentLoaded',() => {
 
   new typeWriter(txtElement, words, wait);
 });
+
+function scrollApper() {
+  const textAnim = document.querySelector('.left-part-why');
+  const mobileAnim = document.querySelector('.right-part-why');
+  const slideAnim = document.querySelector('.slides');
+  const whyPosition = textAnim.getBoundingClientRect().top;
+  const servicePosition = slideAnim.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.4;
+  console.log(screenPosition)
+  if(whyPosition < screenPosition) {
+    textAnim.style.opacity = '1';
+    textAnim.style.transform = 'translateY(0)';
+    mobileAnim.style.opacity = '1';
+    mobileAnim.style.transform = 'translateX(0)';
+  }
+ if(servicePosition < screenPosition) {
+    slideAnim.style.opacity = '1';
+    slideAnim.style.transform = 'translateY(0)';
+  }
+};
+
+window.addEventListener('scroll', scrollApper);
