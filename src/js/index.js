52f31846de292965e1fd01c676cf2221d989e1a9
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-02-04 22:52:20 +0100

    Added gallery img btn

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import textWriter from './typewriter';
 import scrollApper from './scrollanim';
 import {changeDot, changeSlide} from './slide';
 
+// scroll animations and typewriter anim
 document.addEventListener('DOMContentLoaded', () => {
   const accordion = document.getElementById('accordion');
   if(!accordion) {
@@ -14,17 +15,16 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-// SLIDER
+// home page slider
 const links = document.querySelectorAll('.nav-link');
-
 links.forEach((link,index)=>{
   link.addEventListener('click',()=>{
     changeDot(link);
     changeSlide(index);
   });
 });
-//gallery popup
 
+//gallery popup
 let galleryImages = document.querySelectorAll('.gallery-item');
 let getLatestOpenedImg;
 let windowWidth = window.innerWidth;
@@ -44,6 +44,11 @@ if(galleryImages) {
       container.appendChild(newImgWindow);
       newImgWindow.setAttribute('class', 'img-window');
 
+      // adding src of image to element
+      let newImg = document.createElement('img');
+      newImgWindow.appendChild(newImg);
+      newImg.setAttribute('src','../images/'+setNewImgUrl);
+
       // create clobe btn
       let closeBtn = document.createElement('a');
       let newCloseText = document.createTextNode('x');
@@ -51,20 +56,49 @@ if(galleryImages) {
       newImgWindow.appendChild(closeBtn);
       closeBtn.setAttribute('class', 'close-btn');
 
+      newImg.addEventListener('load',() => {
+        // next btn
+        let imgWidth = newImg.width;
+        let caclImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
+
+        let nextBtn = document.createElement('a');
+        let nextText = document.createTextNode('next');
+        nextBtn.appendChild(nextText);
+        container.appendChild(nextBtn);
+        nextBtn.setAttribute('class', 'img-btn-next');
+        nextBtn.addEventListener('click',changeImg(1));
+        nextBtn.style.cssText = 'right:' + caclImgToEdge + 'px;' ;
+
+        // prev btn
+        let prevBtn = document.createElement('a');
+        let prevText = document.createTextNode('prev');
+        prevBtn.appendChild(prevText);
+        container.appendChild(prevBtn);
+        prevBtn.setAttribute('class', 'img-btn-prev');
+        prevBtn.addEventListener('click',changeImg(0));
+        prevBtn.style.cssText = 'left:' + caclImgToEdge + 'px;' ;
+
+      })
+
+      // changing img
+      function changeImg(changeImg) {
+
+      }
+
       // close on btn
       closeBtn.addEventListener('click',()=>{
         newImgWindow.remove();
+        document.querySelector('.img-btn-next').remove();
+        document.querySelector('.img-btn-prev').remove();
       })
       // funcstion to close modal if outside click
       newImgWindow.addEventListener('click',(e)=>{
         if(e.target == newImgWindow) {
           newImgWindow.remove();
+          document.querySelector('.img-btn-next').remove();
+          document.querySelector('.img-btn-prev').remove();
         }
       })
-      // adding src of image to element
-      let newImg = document.createElement('img');
-      newImgWindow.appendChild(newImg);
-      newImg.setAttribute('src','../images/'+setNewImgUrl);
     });
   });
 }
