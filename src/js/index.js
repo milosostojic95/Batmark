ad549ee1b5cf94c124221f9f96f3af1aa231d926
Milos Ostojic <milos.ostojic@newtec-solutions.com>
2021-03-28 20:14:22 +0200

    Home UI WIP

--- src/js/index.js
+++ src/js/index.js
@@ -24,67 +24,19 @@ import '../scss/main.scss';
       margin: 10,
       autoplayHoverPause: true,
       dots: true,
-   
+
+    });
+    var owlTestemonial = $('.studies-testemonials .testemonial-slider .owl-carousel');
+    owlTestemonial.owlCarousel({
+      loop: false,
+      margin: 10,
+      animateIn: 'fadeIn',
+      nav: true,
+      mouseDrag: false,
+      items: 1,
+      navText: ["<img class='arrow-left'/>", "<img class='arrow-right'/>"],
     })
-    var testimonialCarousel = $('.why-batmark .testimonial-slider .owl-carousel');
-    testimonialCarousel.owlCarousel({
-      loop: true,
-      autoplay: false,
-      smartSpeed: 1500,
-      margin: 30,
-      autoWidth: true,
-      autoplayHoverPause: true,
-      dots: true,
-      responsive: {
-        0: {
-          items: 2,
-        },
-        576: {
-          items: 2,
-        },
-        768: {
-          items:2,
-        },
-        992: {
-          items:1,
-        },
-        1200: {
-          items:1,
-        }
-      }
-    })
-    var trainingslCarousel = $('.trainings-testimonial .testimonial-slider-trainings .owl-carousel');
-    trainingslCarousel.owlCarousel({
-      items:2,
-      loop: true,
-      autoplay: false,
-      smartSpeed: 1500,
-      autoWidth: true,
-      autoplayHoverPause: true,
-      center: true,
-      dots: true,
-      responsive: {
-        0: {
-          items: 2,
-          margin: 20,
-        },
-        578: {
-          items: 2,
-          margin: 30,
-        },
-        768: {
-          items: 2,
-          margin: 30,
-        },
-        992: {
-          items: 2,
-          margin: 50,
-        },
-        1200: {
-          margin: 70,
-        }
-      }
-    })
+
     var trainingslCarousel = $('.gallery-slider .testimonial-slider-gallery .owl-carousel');
     trainingslCarousel.owlCarousel({
       items:2,
